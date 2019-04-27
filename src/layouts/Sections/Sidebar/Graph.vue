75219bc107a83d7a8ea4691e7fca3817f7306b68
Prakash <prakash.hartron@gmail.com>
2019-04-27 13:19:11 +0530

    Graph Added

--- src/layouts/Sections/Sidebar/Graph.vue
+++ src/layouts/Sections/Sidebar/Graph.vue
@@ -1,38 +1,126 @@
 <template>
 	<div class="">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <div class="row p-3 pb-0">
-                        <div class="col-6">
-                            <span class="text-small">{{$route.params.workspace}}</span>
-                        </div>
-                        <div class="col-6 text-right">
-                            <span class="fa fa-plus pl-1 pr-1"></span>
-                            <span class="fa fa-expand pl-1 pr-1" @click="sidebarResizeFull"></span>
-                            <span class="fa fa-angle-double-left text-link pl-1 pr-1" @click="sidebarResizeNormal"></span>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-            <ul class="nav nav-action flex-column">
-                <li class="nav-item">
-					<div class="nav-link">
-						<h3>Graph</h3>
-					</div>
-                </li>
-            </ul>
-        </div>
+		<div class="d-flex pt-2 pb-2">
+			<div class="col-6">
+				<span class="text-small">{{$route.params.workspace}}</span>
+			</div>
+			<div class="col-6 text-right">
+				<span class="fa fa-plus pl-1 pr-1"></span>
+				<span class="fa fa-expand pl-1 pr-1" @click="sidebarResizeFull"></span>
+				<span class="fa fa-angle-double-left text-link pl-1 pr-1" @click="sidebarResizeNormal"></span>
+			</div>
+		</div>
+		<div class="d-flex">
+			<div>
+				<d3-network  :net-nodes="nodes" :net-links="links" :options="options" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-	import { mapMutations } from 'vuex';
+import { mapMutations } from 'vuex';
+import D3Network from 'vue-d3-network'
 
-	export default {
-		methods: {
-			...mapMutations([
-				'sidebarResizeFull',
-				'sidebarResizeNormal'
-			])
-		},
-	}
+export default {
+	components: {
+		D3Network
+	},
+	data: function() {
+		return {
+			nodes: [{
+					id: 1,
+					name: 'paras',
+					_color: '#fa5560',
+					_size: 26,
+					_labelClass: 'text-red',
+					_cssClass: 'text-red'
+				},
+				{
+					id: 2,
+					name: 'rocks',
+					_size: 16,
+					_color: '#fa5560',
+					_labelClass: 'text-red',
+					_cssClass: 'text-red'
+				},
+				{
+					id: 3,
+					name: 'danger',
+					_size: 16,
+					_color: '#fa5560',
+					_labelClass: 'text-red',
+					_cssClass: 'text-red'
+				},
+				{
+					id: 4,
+					name: 'love',
+					_color: '#fa5560',
+				},
+				{
+					id: 5,
+					name: 'game',
+					_size: 16,
+					_color: '#fa5560',
+				},
+				{
+					id: 6,
+					name: 'code',
+					_color: '#fa5560',
+				}
+			],
+			options: {
+				nodeLabels: true,
+				linkWidth: 1,
+				strLinks: true,
+				nodeSize: 10,
+				fontSize: 16,
+				force: 3000,
+			},
+			links: [{
+					_color: '#ddd',
+					name: 'paras',
+					tid: 2,
+					sid: 1
+				},
+				{
+					_color: '#ddd',
+					name: 'paras',
+					tid: 1,
+					sid: 5
+				},
+				{
+					_color: '#ddd',
+					name: 'paras',
+					tid: 1,
+					sid: 5
+				},
+				{
+					_color: '#ddd',
+					name: 'paras',
+					tid: 2,
+					sid: 4
+				},
+				{
+					_color: '#ddd',
+					name: 'paras',
+					tid: 2,
+					sid: 6
+				},
+				{
+					_color: '#ddd',
+					name: 'paras',
+					tid: 1,
+					sid: 3
+				}
+			]
+		}
+	},
+	methods: {
+		...mapMutations([
+			'sidebarResizeFull',
+			'sidebarResizeNormal'
+		])
+	},
+}
 </script>
